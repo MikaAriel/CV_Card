8343de2ffd8765050f9e05a11f4450fac117d5c8
MikaAriel <very0512@naver.com>
2015-06-09 11:55:40 +0900

    OZ쓰레시홀

--- CV_Card/CV_CardDlg.cpp
+++ CV_Card/CV_CardDlg.cpp
@@ -7,11 +7,11 @@
 #include "CV_CardDlg.h"
 #include "afxdialogex.h"
 #include "vfw.h"
+#include "ColorConv.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
-
 
 // CCV_CardDlg 대화 상자
 
@@ -41,35 +41,209 @@ BEGIN_MESSAGE_MAP(CCV_CardDlg, CDialogEx)
 	ON_WM_QUERYDRAGICON()
 END_MESSAGE_MAP()
 
+void Otzu_Threshold(unsigned char * orgImg, unsigned char *outImg, int height, int width)
+{
+	register int  i, t;
 
+	//histogram set
+	int hist[256];
+	float prob[256];
+	for (i = 0; i < 256; i++)
+	{
+		hist[i] = 0;
+		prob[i] = 0.0f;
+	}
+	for (i = 0; i < height*width; i++)
+		hist[(int)orgImg[i]]++;
+	for (i = 0; i < 256; i++)
+		prob[i] = (float)hist[i] / (float)(height*width);
+
+	float wsv_min = 100000.0f;
+	float wsv_u1, wsv_u2, wsv_s1, wsv_s2;
+	int wsv_t;
+
+	for (t = 0; t < 256; t++)
+	{
+		//q1,q2 계산
+		float q1 = 0.0f, q2 = 0.0f;
+
+		for (i = 0; i < t; i++)
+			q1 += prob[i];
+		for (i = t; i < 256; i++)
+			q2 += prob[i];
+		if (q1 == 0 || q2 == 0)
+			continue;
+
+		//u1, u2 계산
+		float u1 = 0.0f, u2 = 0.0f;
+		for (i = 0; i < t; i++)
+			u1 += i*prob[i];
+		u1 /= q1;
+		for (i = t; i < 256; i++)
+			u2 += i*prob[i];
+		u2 /= q2;
+
+		//s1,s2 계산
+		float s1 = 0.0f, s2 = 0.0f;
+		for (i = 0; i < t; i++)
+			s1 += (i - u1)*(i - u1)*prob[i];
+		s1 /= q1;
+		for (i = t; i < 256; i++)
+			s2 += (i - u2)*(i - u2)*prob[i];
+		s2 /= q2;
+
+		float wsv = q1*s1 + q2*s2;
+
+		if (wsv < wsv_min)
+		{
+			wsv_min = wsv;
+			wsv_t = t;
+			wsv_u1 = u1;
+			wsv_u2 = u2;
+			wsv_s1 = s1;
+			wsv_s2 = s2;
+		}
+	}
+
+	for (i = 0; i < height*width; i++)
+	{
+		if (orgImg[i] < wsv_t)
+			outImg[i] = 0;
+		else
+			outImg[i] = 255;
+	}
+}
+
+VOID RGB2Gray(char *InImg, char *OutImg, int height, int width)
+{
+	int iCntX;
+	int iCntY;
+	int iJump;
+	unsigned char r, g, b, gray;
+	iJump = 0;
+	for (iCntY = 0; height > iCntY; ++iCntY)
+	{  // RGB값을 찾아서 변환해 주는 곳
+		for (iCntX = 0; width > iCntX; ++iCntX, iJump += 3)
+		{
+			b = InImg[iJump];    // Blue
+			g = InImg[iJump + 1];  // Green
+			r = InImg[iJump + 2];  // Red
+
+			gray = (r + b + g) / 3;
+
+			OutImg[iJump] = gray;
+			OutImg[iJump + 1] = gray;
+			OutImg[iJump + 2] = gray;
+		}
+	}
+}
+VOID RGB2Binary(char *InImg, char *OutImg, int height, int width)
+{
+	int iCntX;
+	int iCntY;
+	int iJump;
+	int i = 0;
+	unsigned char* tempArray = new unsigned char[height * width];
+	unsigned char* outArray = new unsigned char[height * width];
+	iJump = 0;
+	for (iCntY = 0; height > iCntY; ++iCntY)
+	{  // RGB값을 찾아서 변환해 주는 곳
+		for (iCntX = 0; width > iCntX; ++iCntX, iJump += 3)
+		{
+			tempArray[i] = InImg[iJump];
+			i++;
+		}
+	}
+	Otzu_Threshold(tempArray, outArray, height, width);
+	i = 0;
+	iJump = 0;
+	for (iCntY = 0; height > iCntY; ++iCntY)
+	{  // RGB값을 찾아서 변환해 주는 곳
+		for (iCntX = 0; width > iCntX; ++iCntX, iJump += 3)
+		{
+			OutImg[iJump] = outArray[i];
+			OutImg[iJump + 1] = outArray[i];
+			OutImg[iJump + 2] = outArray[i];
+			i++;
+		}
+	}
+	/*for (iCntY = 0; height > iCntY; ++iCntY)
+	{  // RGB값을 찾아서 변환해 주는 곳
+		for (iCntX = 0; width > iCntX; ++iCntX, iJump += 3)
+		{
+			binary = (InImg[iJump] + InImg[iJump + 1] + InImg[iJump + 2]) / 3;
+			if (binary < 128)
+				binary = 0;
+			else
+				binary = 255;
+			OutImg[iJump] = binary;
+			OutImg[iJump + 1] = binary;
+			OutImg[iJump + 2] = binary;
+		}
+	}*/
+	delete[] tempArray;
+	delete[] outArray;
+}
 // CCV_CardDlg 메시지 처리기
 LRESULT CALLBACK VideoCallback(HWND hwnd, LPVIDEOHDR lpVHdr)
 {
 	char *p = (char *)(lpVHdr->lpData);
-
-
+	char *pGray = new char[lpVHdr->dwBytesUsed];
+	char *pBinary = new char[lpVHdr->dwBytesUsed];
 	CDC *pdc = dp->GetDC(); //윈도우 DC를 얻음
 
+	int a = lpVHdr->dwBytesUsed;
 
 	BITMAPINFOHEADER *bmp = (BITMAPINFOHEADER *)bmp_color_head;
-
+	
 	StretchDIBits(
-		pdc->m_hDC,              // hDC
-		0,                                // DestX
-		0,                                // DestY
-		bmp->biWidth,             // nDestWidth
-		bmp->biHeight,            // nDestHeight
-		0,                                // SrcX
-		0,                                // SrcY
-		bmp->biWidth,             // SrcW
-		bmp->biHeight,            // SrcH
-		p,                               // lpBits, 영상데이타
-		(LPBITMAPINFO)bmp,  // lpBitsInfo
+		pdc->m_hDC,				// hDC
+		0,						// DestX
+		0,						// DestY
+		bmp->biWidth,			// nDestWidth
+		bmp->biHeight,			// nDestHeight
+		0,						// SrcX
+		0,						// SrcY
+		bmp->biWidth,			// SrcW
+		bmp->biHeight,			// SrcH
+		p,						// lpBits, 영상데이터
+		(LPBITMAPINFO)bmp,		// lpBitsInfo
 		DIB_RGB_COLORS,
-		SRCCOPY);               // wUsage
-
+		SRCCOPY);				// wUsage*/
+	RGB2Gray(p, pGray, bmp->biHeight, bmp->biWidth);
+	StretchDIBits(
+		pdc->m_hDC,				// hDC
+		320,					// DestX
+		0,						// DestY
+		bmp->biWidth,			// nDestWidth
+		bmp->biHeight,			// nDestHeight
+		0,						// SrcX
+		0,						// SrcY
+		bmp->biWidth,			// SrcW
+		bmp->biHeight,			// SrcH
+		pGray,					// lpBits, 영상데이터
+		(LPBITMAPINFO)bmp,		// lpBitsInfo
+		DIB_RGB_COLORS,
+		SRCCOPY);				// wUsage*/
+	RGB2Binary(pGray, pBinary, bmp->biHeight, bmp->biWidth);
+	StretchDIBits(
+		pdc->m_hDC,				// hDC
+		0,						// DestX
+		240,					// DestY
+		bmp->biWidth,			// nDestWidth
+		bmp->biHeight,			// nDestHeight
+		0,						// SrcX
+		0,						// SrcY
+		bmp->biWidth,			// SrcW
+		bmp->biHeight,			// SrcH
+		pBinary,				// lpBits, 영상데이터
+		(LPBITMAPINFO)bmp,		// lpBitsInfo
+		DIB_RGB_COLORS,
+		SRCCOPY);				// wUsage*/
 	dp->ReleaseDC(pdc);
 
+	delete[] pGray;
+	delete[] pBinary;
 	return (LRESULT)TRUE;
 }
 //
@@ -82,7 +256,7 @@ BOOL CCV_CardDlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
 	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
-	ShowWindow(SW_MAXIMIZE);
+	ShowWindow(SW_SHOW);
 
 	// TODO: 여기에 추가 초기화 작업을 추가합니다.
 
@@ -137,10 +311,10 @@ BOOL CCV_CardDlg::OnInitDialog()
 
 	// capture영상의 크기를 320x240으로 둔다.
 	BITMAPINFOHEADER *bmp = (BITMAPINFOHEADER *)bmp_color_head;
-	bmp->biWidth = 800;
-	bmp->biHeight = 600;
+	bmp->biWidth = 320;
+	bmp->biHeight = 240;
 	capSetVideoFormat(d_cap, bmp, 40);
-
+	capOverlay(d_cap, TRUE);
 	capSetCallbackOnVideoStream(d_cap, &VideoCallback);
 	capSetCallbackOnFrame(d_cap, &VideoCallback);  // also use for single 
 
@@ -196,7 +370,6 @@ BOOL CCV_CardDlg::DestroyWindow()
 		FreeProcInstance(VideoCallback);
 		//		VideoCallback = NULL; 
 	}
-
 	// Disconnect the current capture driver 
 	capCaptureStop(d_cap);
 	capDriverDisconnect(d_cap);
